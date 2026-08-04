f5643cc1e71cd58f4f84db8d9ac786bdb0e41363
ahmedfarghaly261 <ahmedfarghaly201055@gmail.com>
2026-08-04 19:47:55 +0300

    add CaptainTournamentEligibilityController to TournamentsModule

--- apps/api/src/modules/tournaments/tournaments.module.ts
+++ apps/api/src/modules/tournaments/tournaments.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { CaptainTournamentEligibilityController } from './captain-tournament-eligibility.controller';
 import { OrganizerTournamentsController } from './organizer-tournaments.controller';
 import { PublicTournamentsController } from './public-tournaments.controller';
 import { TournamentsService } from './tournaments.service';
 
 @Module({
-  controllers: [OrganizerTournamentsController, PublicTournamentsController],
+  controllers: [
+    OrganizerTournamentsController,
+    CaptainTournamentEligibilityController,
+    PublicTournamentsController,
+  ],
   providers: [TournamentsService],
 })
 export class TournamentsModule {}
