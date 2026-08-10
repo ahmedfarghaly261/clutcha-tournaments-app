86a0b3a1f5a41220544f32631e6d7a2c6952b59c
ahmedfarghaly261 <ahmedfarghaly201055@gmail.com>
2026-08-10 14:08:47 +0300

    feat: add registration module exports for RegistrationExperience and services

---
+++ apps/web/src/modules/auth/registration/index.ts
@@ -0,0 +1,3 @@
+export { RegistrationExperience } from './components/RegistrationExperience'
+export { useRegistrationService } from './services/registration.service'
+export type { RegistrationAccountType } from './schemas/registration.schema'
