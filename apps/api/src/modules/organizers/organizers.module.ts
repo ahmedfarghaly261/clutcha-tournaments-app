ca6318ef3e241ebff057374c5efa3ef29e6ff58f
ahmedfarghaly261 <ahmedfarghaly201055@gmail.com>
2026-08-11 11:10:58 +0300

    feat: add OrganizerProfileImageStorageService to OrganizersModule providers

--- apps/api/src/modules/organizers/organizers.module.ts
+++ apps/api/src/modules/organizers/organizers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { OrganizerProfileImageStorageService } from './organizer-profile-image-storage.service';
 import { OrganizersController } from './organizers.controller';
 import { OrganizersService } from './organizers.service';
 
 @Module({
   controllers: [OrganizersController],
-  providers: [OrganizersService],
+  providers: [OrganizersService, OrganizerProfileImageStorageService],
 })
 export class OrganizersModule {}
