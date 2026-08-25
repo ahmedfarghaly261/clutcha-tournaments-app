8ea8aa63ca3139442527c2c546cdffba2bdf03dd
ahmedfarghaly261 <ahmedfarghaly201055@gmail.com>
2026-08-25 14:02:36 +0300

    feat: add useCaptainTeamRegionsService to retrieve team regions

--- apps/web/src/modules/captain/team/services/captain-team.service.ts
+++ apps/web/src/modules/captain/team/services/captain-team.service.ts
@@ -1,10 +1,21 @@
-import { useCaptainsControllerGetTeam } from '@/api/generated/captain/captain'
+import {
+  useCaptainsControllerGetTeam,
+  useCaptainsControllerListTeamRegions,
+} from '@/api/generated/captain/captain'
 
 export function useCaptainTeamService() {
   return useCaptainsControllerGetTeam({
     query: {
       retry: false,
       staleTime: 15_000,
+    },
+  })
+}
+
+export function useCaptainTeamRegionsService() {
+  return useCaptainsControllerListTeamRegions({
+    query: {
+      staleTime: 60_000,
     },
   })
 }
